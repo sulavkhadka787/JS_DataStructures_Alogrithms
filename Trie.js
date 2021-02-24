5b11c802c6a962d2e7d5cc1bafe7df5b1542775e
Sulav Khadka <sulav.khadka787@gmail.com>
2021-02-24 15:25:00 -0700

    Trie add function

---
+++ Trie.js
@@ -0,0 +1,29 @@
+/* Trie Data Structure */
+
+let Node = function() {
+	this.keys = new Map();
+	this.end = false;
+	this.setEnd = function() {
+		this.end = true;
+	};
+	this.isEnd = function() {
+		return this.end;
+	};
+};
+
+let Trie = function() {
+
+	this.root = new Node();
+
+	this.add = function(input, node = this.root) {
+		if (input.length == 0) {
+			node.setEnd();
+			return;
+		} else if (!node.keys.has(input[0])) {
+			node.keys.set(input[0], new Node());
+			return this.add(input.substr(1), node.keys.get(input[0]));
+		} else {
+			return this.add(input.substr(1), node.keys.get(input[0]));
+		};
+	};
+}
