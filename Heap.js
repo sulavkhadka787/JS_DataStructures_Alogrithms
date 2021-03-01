2ffd1fec66e9e67f7ed3106a7be309ff1a2f31af
Sulav Khadka <sulav.khadka787@gmail.com>
2021-03-01 09:16:23 -0700

    remove element from minheap

--- Heap.js
+++ Heap.js
@@ -24,5 +24,40 @@ let MinHeap=function(){
         }
     }
 
-    
+    this.remove=function(){
+        let smallest=heap[1];
+        if(heap.length>2){
+            heap[1]=heap[heap.length-1];
+            heap.splice(heap.length-1);
+            if(heap.length==3){
+                if(heap[1]>heap[2]){
+                    [heap[1],heap[2]]=[heap[2],heap[1]];
+                }
+                return smallest;
+            };
+
+            let i=1;
+            let left=2*i;
+            let right=2*i+1;
+            while(heap[i]>=heap[left] ||heap[i] >=heap[right]){
+                if(heap[left]<heap[right]){
+                    [heap[i],heap[left]]=[heap[left],heap[i]];
+                    i=2*1;
+                }else{
+                    [heap[i],heap[right]]=[heap[right],heap[i]];
+                    i=2*1+1;
+                }
+                left=2*i;
+                right=2*1+i;
+                if(heap[left]==undefined || heap[right]==undefined){
+                    break;
+                }
+            }
+        }else if(heap.length==2){
+            heap.splice(1,1);
+        }else{
+            return null;
+        };
+        return smallest;
+    };
 }
